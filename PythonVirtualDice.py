04657387550f8ae2a404564ef9e1bd21f8876f9a
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-04-01 20:24:01 -0500

    Strange bug in the Damage dice
    Not sure what's up with the Damage dice.  They have a disproportionately high tendency to roll minimum damage.  I've seen them roll high exactly twice in about 50 rolls.  Need to investigate further.

--- PythonVirtualDice.py
+++ PythonVirtualDice.py
@@ -10,25 +10,27 @@ import sys
 
 while True:
 
-	choice = input("[A]ttack roll, [D]amage roll, [S]aving Throw, or S[k]]ill check ").lower
-	if choice == ('a' or 'attack'):
+	choice = input("[A]ttack roll, [D]amage roll, [S]aving Throw, or S[k]]ill check ").lower()
+	if choice == 'a':
 		atp = input("Enter your attack power: ")
 		dice = randint(1, 20) + int(atp)
-		armck = input(f"You rolled a {dice}!  Roll damage? (y/n): ")
+		armck = input(f"You rolled a {dice}!  Roll damage? (y/n): ").lower()
 		if armck == 'y':
 			diceNum = int(input("How many dice will you throw? "))
 			diceType = int(input("What type of dice (D4, D12, etc): D"))
 		print(f"{randint({diceNum}, {diceType})}")
-	elif choice == 'd' or 'damage':
+	elif choice == ('d' or 'damage'):
 		diceNum = int(input("how many dice? "))
 		diceType = int(input("What type? (D8, D12, etc): D "))
-		dmgBonus = int(input("Enter any damage bonus(es) or 0 if none: 1"))
-		print(f"You rolled a {randint(diceNum, diceType) + dmgBonus}")
-	elif choice == 's' or 'save' or 'saving throw':
+		dmgBonus = int(input("Enter any damage bonus(es) or 0 if none): "))
+		print(f"{randint(diceNum, diceType) + dmgBonus} damage!")
+	elif choice == 's':
 		saving = int(input("Add your save modifier: "))
 		print(f"Your save is {randint(1, 20) + saving}")
-	elif choice == 'k' or 'skill' or 'skill check':
+	elif choice == 'k':
 		skill = int(input("Enter your skill level: "))
 		print(f"Your skill check roll was: {randint(1, 20) + skill}")
-	elif choice == 'Quit' or 'quit' or 'Q' or 'q' or 'Exit' or 'exit':
+	elif choice == 'q':
 		sys.exit("Thanks for playing!")
+	else:
+		print("Invalid command.")
---
+++ dicetest.py
@@ -0,0 +1,6 @@
+from random import randint
+while True:
+    diceNum = int(input("how many dice? "))
+    diceType = int(input("What type? (D8, D12, etc): D"))
+    dmgBonus = int(input("Enter any damage bonus(es) or 0 if none): "))
+    print(f"{randint(diceNum, diceType) + dmgBonus} damage!")
