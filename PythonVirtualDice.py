b81340c20b71e4e08b7e7f55d3edb820387a720c
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-04-02 04:45:12 -0500

    Version 2 is complete!
    In version 2 you can now choose "Classic" mode where you just roll 1 of whatever di you want, or 'guided' D20 mode for organized TTRPG groups.
    Hint: If you need a coin flip for RP or what-have-you, use Damage, 1d2+0 :)

--- PythonVirtualDice.py
+++ PythonVirtualDice.py
@@ -1,37 +1,36 @@
 from random import randint
 import sys
-#
-# while True:
-#		dice = input("Roll the dice ")
-#	if dice == "q":
-#		break
-#	else:
-#		print(f"You have rolled a d{dice}: {randint(1, int(dice))}")
 
-while True:
-
-	choice = input("[A]ttack roll, [D]amage roll, [S]aving Throw, or S[k]]ill check ").lower()
-	if choice == 'a':
-		atp = input("Enter your attack power: ")
-		dice = randint(1, 20) + int(atp)
-		armck = input(f"You rolled a {dice}!  Roll damage? (y/n): ").lower()
-		if armck == 'y':
-			diceNum = int(input("How many dice will you throw? "))
-			diceType = int(input("What type of dice (D4, D12, etc): D"))
-			dmgBonus = int(input("Apply any damage bonus (or type '0' if none): "))
-		print(f"{randint(diceNum, (diceNum * diceType) + dmgBonus)} damage!")
-	elif choice == ('d' or 'damage'):
-		diceNum = int(input("how many dice? "))
-		diceType = int(input("What type? (D8, D12, etc): D "))
-		dmgBonus = int(input("Enter any damage bonus(es) or 0 if none): "))
-		print(f"{randint(diceNum, (diceNum * diceType)) + dmgBonus} damage!")
-	elif choice == 's':
-		saving = int(input("Add your save modifier: "))
-		print(f"Your save is {randint(1, 20) + saving}")
-	elif choice == 'k':
-		skill = int(input("Enter your skill level: "))
-		print(f"Your skill check roll was: {randint(1, 20) + skill}")
-	elif choice == 'q':
-		sys.exit("Thanks for playing!")
-	else:
-		print("Invalid command.")
+gameType = input("""Greetings, player!  Do you want [C]lassic dice where you can choose any dice you want,
+or would you like a more guided, [D]20 experience? """).lower()
+if gameType == 'c' or 'classic':
+	while True:
+		dice = input("Roll the dice.  Enter any whole number to get 1D of that type, or Q to exit: ")
+		if dice == "q":
+			sys.exit("Thanks for playing!")
+		else:
+			print(f"You have rolled a d{dice}: {randint(1, int(dice))}")
+elif gameType == 'd':
+	while True:
+		choice = input("[A]ttack roll, [D]amage roll, or [S]aving Throw/S[k]]ill check ").lower()
+		if choice == 'a' or 'attack' or 'attack roll':
+			atp = input("Enter your attack power: ")
+			dice = randint(1, 20) + int(atp)
+			armck = input(f"You rolled a {dice}!  Roll damage? (y/n): ").lower()
+			if armck == 'y':
+				diceNum = int(input("How many dice will you throw? "))
+				diceType = int(input("What type of dice (D4, D12, etc): D"))
+				dmgBonus = int(input("Apply any damage bonus (or type '0' if none): "))
+				print(f"{randint(diceNum, (diceNum * diceType)) + dmgBonus} damage!")
+		elif choice == 'd' or 'damage' or 'damage roll':
+			diceNum = int(input("how many dice? "))
+			diceType = int(input("What type? (D8, D12, etc): D "))
+			dmgBonus = int(input("Enter any damage bonus(es) or 0 if none): "))
+			print(f"{randint(diceNum, (diceNum * diceType)) + dmgBonus} damage!")
+		elif choice == 's' or 'save' or 'saving' or 'saving throw' or 'k' or 'skill' or 'skill check':
+			savingSkill = int(input("Add your save/skill modifier: "))
+			print(f"Your check result is {randint(1, 20) + savingSkill}")
+		elif choice == 'q' or 'quit' or 'e' or 'exit':
+			sys.exit("Thanks for playing!")
+		else:
+			print("Invalid command.")
